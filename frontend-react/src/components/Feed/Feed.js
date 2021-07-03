8d756e572396fb5bb7336a87b85566f6bdf4ce2d
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-07-03 21:33:49 +0500

    done with the post section under feed

--- frontend-react/src/components/Feed/Feed.js
+++ frontend-react/src/components/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Feed.css'
 import MessageSender from './MessageSender/MessageSender'
+import Post from './Post/Post'
 import StoryReel from './StoryReel/StoryReel'
 
 function Feed() {
@@ -8,6 +9,16 @@ function Feed() {
         <div className="feed">
             <StoryReel/>
             <MessageSender/>
+
+            <Post 
+              username="Muhammad Muneeb"
+              message="Wow this is legit"
+              timestamp="This is my time"
+              image="https://links.papareact.com/xql"
+              profilePic="https://scontent.fhdd3-1.fna.fbcdn.net/v/t1.6435-9/91601480_2654970644785952_2808636843233378304_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=BHomamLBVRUAX-v1Q0T&_nc_ht=scontent.fhdd3-1.fna&oh=228f8de1b4b7f4b32b09d0b55f483306&oe=60E4C3BD"
+            />
+            {/* <Post/>
+            <Post/> */}
         </div>
     )
 }
---
+++ frontend-react/src/components/Feed/Post/Post.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Post.css'
+import { Avatar } from '@material-ui/core'
+import { AccountCircle, ChatBubbleOutline, ExpandMoreOutlined, NearMe, ThumbsUpDown, ThumbUp } from '@material-ui/icons'
+
+
+function Post({ profilePic, image, username, timestamp, message }) {
+    return (
+        <div className="post">
+
+            <div className="post__top">
+                <Avatar src={profilePic} className="post__avatar"/>
+                <div className="post__topInfo">
+                    <h3>{username}</h3>
+                    <p>Timestamp</p>
+                </div>
+            </div>
+
+            <div className="post__bottom">
+                <p>{message}</p>
+            </div>
+
+            <div className="post__image">
+                <img src={image} />
+            </div>
+
+            <div className="post__options">
+                <div className="post__option">
+                    <ThumbUp/>
+                    <p>Like</p>
+                </div>
+                <div className="post__option">
+                    <ChatBubbleOutline/>
+                    <p>Comment</p>
+                </div>
+                <div className="post__option">
+                    <NearMe/>
+                    <p>Share</p>
+                </div>
+                <div className="post__option">
+                    <AccountCircle/>
+                    <ExpandMoreOutlined/>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Post
