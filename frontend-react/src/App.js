072d6f04980b4d13cc28802316319d64ce691376
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-07-03 22:32:59 +0500

    bump widgets section. UI almost completed

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import Widgets from './components/Widgets/Widgets';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <div className="app__body">
         <Sidebar/>
         <Feed/>
-        {/*widgets*/}
+        <Widgets/>
       </div>
 
 
---
+++ frontend-react/src/components/Widgets/Widgets.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Widgets.css'
+
+function Widgets() {
+    return (
+        <div className="widget">
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCleverProgrammerr&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="100%" style={{border: "none", overflow: "hidden"}} scrolling="no" frameBorder="0" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        </div>
+    )
+}
+
+export default Widgets
