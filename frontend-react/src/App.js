76da10e29122da6a01328ee80aad90f2d7d75128
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-07-08 11:16:30 +0500

    added sigin functionality

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -3,29 +3,32 @@ import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import Login from './components/Login/Login';
 import Sidebar from './components/Sidebar/Sidebar';
+import { useStateValue } from './components/StateProvider/StateProvider';
 import Widgets from './components/Widgets/Widgets';
 
 
 function App() {
-  const user = "muneeb"
+  const [{user}, dispatch] = useStateValue();
     return (
-    <div className="app">
-      {!user ? (
-        <Login/>
-      ):(
-        <>
-        <Header/>
 
-        <div className="app__body">
-          <Sidebar/>
-          <Feed/>
-          <Widgets />
-        </div>
-        </>
-      )}
+    
+      <div className="app">
+        {!user ? (
+          <Login/>
+        ):(
+          <>
+          <Header/>
 
-    </div>
-  );
-}
+          <div className="app__body">
+            <Sidebar/>
+            <Feed/>
+            <Widgets />
+          </div>
+          </>
+        )}
+
+      </div>
+    );
+    }
 
 export default App;
--- frontend-react/src/components/Login/Login.js
+++ frontend-react/src/components/Login/Login.js
@@ -2,14 +2,21 @@ import { Button } from '@material-ui/core'
 import React from 'react'
 import './Login.css'
 import {auth, provider} from '../../firebase';
+import { useStateValue } from '../StateProvider/StateProvider';
+import { actionTypes } from '../StateProvider/Reducer';
 
 function Login() {
+    const [state, dispatch] = useStateValue();
     
     const signIn = () => {
         auth
         .signInWithPopup(provider)
         .then((result)=>{
-            console.log(result)
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.user
+            })
+            
         })
         .catch((error)=>alert(error.message))
     };
