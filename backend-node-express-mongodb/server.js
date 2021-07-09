9bb1b962f307ce55e3f710c7c043a524273cf230
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-07-09 19:22:07 +0500

    post and retrieve image

--- backend-node-express-mongodb/server.js
+++ backend-node-express-mongodb/server.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import path from 'path';
 import Grid from 'gridfs-stream';
 import multer from 'multer';
-import GridFsStorage from 'multer-gridfs-storage';
+import GridFsStorage from 'multer-gridfs-storage'
 import bodyParser from 'body-parser';
 
 Grid.mongo = mongoose.mongo
@@ -33,7 +33,7 @@ mongoose.connection.once('open', ()=>{
     console.log('db connected')
 })
 
-//all below work for images
+// *****************all below work for images ****
 const conn = mongoose.createConnection(mongoURI, {
     useCreateIndex:true,
     useNewUrlParser:true,
@@ -47,7 +47,7 @@ conn.once('open', ()=>{
     console.log('db connected')
     gfs = Grid(conn.db, mongoose.mongo)
     gfs.collection('images')
-})
+});
 
 const storage = new GridFsStorage({
     url:mongoURI,
@@ -72,6 +72,29 @@ const upload = multer({ storage })
 
 //api routes
 app.get('/', (req, res)=>res.status(200).send('hello world'))
+
+
+//***********getting image*********
+
+app.get('/retrieve/image/single', (req, res)=>{
+    gfs.files.findOne({filename: req.query.name }, (err, file)=>{
+        if (err) {
+            res.status(500).send(err)
+        }else {
+            if (!file || file.length == 0) {
+                res.status(404).json({err: 'file not found'})
+            } else{
+                const readstream = gfs.createReadStream(file.filename);
+                readstream.pipe(res);
+            }
+        }
+    })
+})
+
+//********************** image post *****************
+app.post('/upload/image', upload.any('file'), (req, res)=>{
+    res.status(201).send(req.file)
+})
 
 //listener
 app.listen(port, console.log(`listening on localhost: ${port}`));
